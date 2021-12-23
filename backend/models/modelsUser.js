72bc5ad307cd891294851f8cdef2ff1086f7c3bd
changriha neijma <changriha.neijma@orange.fr>
2021-12-23 23:50:46 +0100

    unique-validator email

--- backend/models/modelsUser.js
+++ backend/models/modelsUser.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
+const uniqueValidator = require("mongoose-unique-validator");
+
 const modelUserSchema = mongoose.Schema({
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
+
+modelUserSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("User", modelUserSchema);
