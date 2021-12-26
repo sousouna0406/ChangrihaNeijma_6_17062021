e594243c36ce05d269356f0e7f2a5d6de80ff88e
changriha neijma <changriha.neijma@orange.fr>
2021-12-26 11:06:19 +0100

    body parser et images

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,18 @@
-// const http = require("http");
-
-// const server = http.createServer((req, res) => {
-//   res.end("Voilà la réponse du serveur !");
-// });
-
-// server.listen(process.env.PORT || 3000);
-
 const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
 const sauceRouting = require("./routes/sauceRoutes");
 const authRouting = require("./routes/authRoutes");
+
 require("./models/dbConfig");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/api/sauces", sauceRouting);
 app.use("/api/auth", authRouting);
 
