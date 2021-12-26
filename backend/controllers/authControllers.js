0e035ec9f8ca25a74bc0c5b54618a3314cc664ce
changriha neijma <changriha.neijma@orange.fr>
2021-12-26 11:06:42 +0100

    jwt correction

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -36,7 +36,7 @@ exports.login = (req, res) => {
           }
           res.status(200).json({
             userId: user._id,
-            token: jwt.sign({ userId: user._id }, "RAMDOM_TOKEN_SECRET", {
+            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
               expiresIn: "24h",
             }),
           });
