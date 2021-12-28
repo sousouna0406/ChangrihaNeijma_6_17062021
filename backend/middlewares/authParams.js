21d85366aa9e1d94e5826ae2a95879257bc95061
changriha neijma <changriha.neijma@orange.fr>
2021-12-28 17:29:10 +0100

    regex email

--- backend/middlewares/authParams.js
+++ backend/middlewares/authParams.js
@@ -1,4 +1,27 @@
 module.exports = (req, res, next) => {
   // je dois verifier le body pour savoir si lemail est bien present et si lemail est bien un email
   // REGEX
+  const bodyEmail = req.body.email;
+  const bodyPassword = req.body.password;
+  if (bodyEmail && bodyPassword) {
+    next();
+  } else if (bodyEmail) {
+    const emailRegex =
+      /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/.test(
+        bodyEmail
+      );
+    if (emailRegex === true) {
+      next();
+    } else {
+      return res.status(401).json({
+        error: "Format Email incorrect",
+      });
+    }
+  } else if (!bodyPassword) {
+    return res.status(401).json({
+      error: "Mettre un mot de passe",
+    });
+  } else {
+    return res.status(400).json({ message: "introuvable" });
+  }
 };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const { signup, login } = require("../controllers/authControllers");
+const authParams = require("../middlewares/authParams");
 
 const router = Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
+router.post("/signup", authParams, signup);
+router.post("/login", authParams, login);
 
 module.exports = router;
